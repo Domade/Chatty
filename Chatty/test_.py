324a4526c587a48ce1791bcbf0b9a815055ed177
Domade <focus.drive.succeed@gmail.com>
2024-01-09 02:09:18 -0600

    Update test_.py

---
+++ Chatty/test_.py
@@ -0,0 +1,27 @@
+def binary_search(lst, target):
+    low = 0
+    high = len(lst) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if lst[mid] == target:
+            return mid
+        elif lst[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+
+while True:
+    text = input("Enter the text that you want to convert to speech: ")
+    if text.lower() == 'quit':
+        break
+    output = speech_synthesizer.speak_text_async(text).get()
+    # rest of the code
+
+
+try:
+    pass  # Placeholder for the code that may raise an exception
+except Exception as e:
+    logging.exception("An error occurred")
